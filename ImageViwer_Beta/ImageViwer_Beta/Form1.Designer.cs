6cec86e6e33e2144972b5c541cd815b25d451128
2k16daniel <2k16daniel@gmail.com>
2019-11-08 21:17:48 +0800

    revert

--- ImageViwer_Beta/ImageViwer_Beta/Form1.Designer.cs
+++ ImageViwer_Beta/ImageViwer_Beta/Form1.Designer.cs
@@ -30,29 +30,47 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Button_PicOpen = new System.Windows.Forms.Button();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.ImageListview = new System.Windows.Forms.ListView();
             this.MainPictureBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openPhotoMenu = new System.Windows.Forms.ToolStripMenuItem();
-            this.propertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.photosMetaDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.fullSizeImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.selectedImagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.prev_btn = new System.Windows.Forms.Button();
-            this.next_btn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).BeginInit();
             this.panel1.SuspendLayout();
-            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Button_PicOpen
+            // 
+            this.Button_PicOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Button_PicOpen.Location = new System.Drawing.Point(12, 463);
+            this.Button_PicOpen.Name = "Button_PicOpen";
+            this.Button_PicOpen.Size = new System.Drawing.Size(104, 32);
+            this.Button_PicOpen.TabIndex = 1;
+            this.Button_PicOpen.Text = "OPEN";
+            this.Button_PicOpen.UseVisualStyleBackColor = true;
+            this.Button_PicOpen.Click += new System.EventHandler(this.Button_PicOpen_Click);
             // 
             // imageList1
             // 
             this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             this.imageList1.Images.SetKeyName(0, "bg2.png");
+            // 
+            // ImageListview
+            // 
+            this.ImageListview.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ImageListview.LargeImageList = this.imageList1;
+            this.ImageListview.Location = new System.Drawing.Point(12, 12);
+            this.ImageListview.Name = "ImageListview";
+            this.ImageListview.Size = new System.Drawing.Size(190, 436);
+            this.ImageListview.TabIndex = 2;
+            this.ImageListview.UseCompatibleStateImageBehavior = false;
+            this.ImageListview.ItemActivate += new System.EventHandler(this.ImageListview_ItemActivate);
+            this.ImageListview.SelectedIndexChanged += new System.EventHandler(this.ImageList_SelectedIndexChanged);
             // 
             // MainPictureBox
             // 
@@ -64,6 +82,17 @@
             this.MainPictureBox.TabStop = false;
             this.MainPictureBox.Click += new System.EventHandler(this.MainPictureBox_Click);
             // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(184, 527);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Info";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // panel1
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -71,108 +100,21 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.AutoScroll = true;
             this.panel1.Controls.Add(this.MainPictureBox);
-            this.panel1.Location = new System.Drawing.Point(108, 34);
+            this.panel1.Location = new System.Drawing.Point(236, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(846, 516);
+            this.panel1.Size = new System.Drawing.Size(840, 538);
             this.panel1.TabIndex = 8;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem,
-            this.propertiesToolStripMenuItem,
-            this.aboutToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1089, 24);
-            this.menuStrip1.TabIndex = 9;
-            this.menuStrip1.Text = "menuStrip1";
-            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
-            // 
-            // fileToolStripMenuItem
-            // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openPhotoMenu});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "File";
-            // 
-            // openPhotoMenu
-            // 
-            this.openPhotoMenu.Name = "openPhotoMenu";
-            this.openPhotoMenu.Size = new System.Drawing.Size(138, 22);
-            this.openPhotoMenu.Text = "Open Photo";
-            this.openPhotoMenu.Click += new System.EventHandler(this.openPhotoMenu_Click);
-            // 
-            // propertiesToolStripMenuItem
-            // 
-            this.propertiesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.photosMetaDataToolStripMenuItem});
-            this.propertiesToolStripMenuItem.Name = "propertiesToolStripMenuItem";
-            this.propertiesToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
-            this.propertiesToolStripMenuItem.Text = "Properties";
-            // 
-            // photosMetaDataToolStripMenuItem
-            // 
-            this.photosMetaDataToolStripMenuItem.Name = "photosMetaDataToolStripMenuItem";
-            this.photosMetaDataToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
-            this.photosMetaDataToolStripMenuItem.Text = "Photo\'s Meta Data";
-            // 
-            // aboutToolStripMenuItem
-            // 
-            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fullSizeImageToolStripMenuItem,
-            this.selectedImagesToolStripMenuItem});
-            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
-            this.aboutToolStripMenuItem.Text = "View";
-            // 
-            // fullSizeImageToolStripMenuItem
-            // 
-            this.fullSizeImageToolStripMenuItem.Name = "fullSizeImageToolStripMenuItem";
-            this.fullSizeImageToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
-            this.fullSizeImageToolStripMenuItem.Text = "Full Size Image";
-            this.fullSizeImageToolStripMenuItem.Click += new System.EventHandler(this.fullSizeImageToolStripMenuItem_Click);
-            // 
-            // selectedImagesToolStripMenuItem
-            // 
-            this.selectedImagesToolStripMenuItem.Name = "selectedImagesToolStripMenuItem";
-            this.selectedImagesToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
-            this.selectedImagesToolStripMenuItem.Text = "Selected Images";
-            this.selectedImagesToolStripMenuItem.Click += new System.EventHandler(this.selectedImagesToolStripMenuItem_Click);
-            // 
-            // prev_btn
-            // 
-            this.prev_btn.Location = new System.Drawing.Point(455, 556);
-            this.prev_btn.Name = "prev_btn";
-            this.prev_btn.Size = new System.Drawing.Size(75, 32);
-            this.prev_btn.TabIndex = 10;
-            this.prev_btn.Text = "Prev";
-            this.prev_btn.UseVisualStyleBackColor = true;
-            this.prev_btn.Click += new System.EventHandler(this.prev_btn_Click);
-            // 
-            // next_btn
-            // 
-            this.next_btn.Location = new System.Drawing.Point(547, 556);
-            this.next_btn.Name = "next_btn";
-            this.next_btn.Size = new System.Drawing.Size(76, 32);
-            this.next_btn.TabIndex = 11;
-            this.next_btn.Text = "Next";
-            this.next_btn.UseVisualStyleBackColor = true;
-            this.next_btn.Click += new System.EventHandler(this.next_btn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1089, 591);
-            this.Controls.Add(this.next_btn);
-            this.Controls.Add(this.prev_btn);
+            this.ClientSize = new System.Drawing.Size(1082, 563);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ImageListview);
+            this.Controls.Add(this.Button_PicOpen);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.ShowInTaskbar = false;
@@ -181,28 +123,18 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).EndInit();
             this.panel1.ResumeLayout(false);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Button Button_PicOpen;
+        private System.Windows.Forms.ListView ImageListview;
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.PictureBox MainPictureBox;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem openPhotoMenu;
-        private System.Windows.Forms.ToolStripMenuItem propertiesToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem fullSizeImageToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem photosMetaDataToolStripMenuItem;
-        private System.Windows.Forms.Button prev_btn;
-        private System.Windows.Forms.Button next_btn;
-        private System.Windows.Forms.ToolStripMenuItem selectedImagesToolStripMenuItem;
     }
 }
 
--- ImageViwer_Beta/ImageViwer_Beta/Form1.cs
+++ ImageViwer_Beta/ImageViwer_Beta/Form1.cs
@@ -1,5 +1,4 @@
 ﻿using MetadataExtractor;
-using MetadataExtractor.Formats.Exif;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -16,8 +15,6 @@ namespace ImageViwer_Beta
 {
     public partial class Form1 : Form
     {
-        protected string[] pFileNames;
-        protected int pCurrentImage = -1;
         List<string> imagelist = new List<string>();
         public Form1()
         {
@@ -33,19 +30,19 @@ namespace ImageViwer_Beta
             // open file dialog   
             OpenFileDialog open = new OpenFileDialog();
             // image filters  
-            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png *.tiff)|*.jpg; *.jpeg; *.gif; *.bmp; *.png; *.tiff";
-            //open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
+            open.Filter = "JPEG|*.jpg|Bitmaps|*.bmp|PNG|*.png|GIF|*.gif|TIFF|*.tiff";
+            
             open.Multiselect = true;
             open.ValidateNames = true;
             if (open.ShowDialog() == DialogResult.OK)
             {
                 imagelist.Clear();
-                //ImageListview.Items.Clear();
+                ImageListview.Items.Clear();
                 foreach (string filename in open.FileNames)
                 {
                     FileInfo imginfo = new FileInfo(filename);
                     imagelist.Add(imginfo.FullName);
-                   // ImageListview.Items.Add(imginfo.Name, 0);
+                    ImageListview.Items.Add(imginfo.Name, 0);
                 }
             }
             //open.Dispose();
@@ -53,7 +50,7 @@ namespace ImageViwer_Beta
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //hello
+
         }
 
         private void ImageList_SelectedIndexChanged(object sender, EventArgs e)
@@ -63,9 +60,15 @@ namespace ImageViwer_Beta
 
         private void ImageListview_ItemActivate(object sender, EventArgs e)
         {
-           /* if (ImageListview.FocusedItem != null)
+            if (ImageListview.FocusedItem != null)
             {
-              
+                /* using (Photo phto = new Photo())
+                 {
+                     Image img = Image.FromFile(imagelist[ImageListview.FocusedItem.Index]);
+                     phto.imagebox = img;
+                     phto.ShowDialog();
+
+                 } */
                 try
                 {
                     Image img = Image.FromFile(imagelist[ImageListview.FocusedItem.Index]);
@@ -73,15 +76,14 @@ namespace ImageViwer_Beta
                 }
                 catch (Exception ex)
                 {
-                    //MessageBox.Show(this, ex.Message, "Invalid File! Please Make sure to choose the valid file!",
-                    //MessageBoxButtons.OK, MessageBoxIcon.Information
-                    MessageBox.Show("Invalid File! Please Make sure to choose the valid file!");
-                    
+                    MessageBox.Show(this, ex.Message, "Invalid File! Please Make sure to choose the valid file!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information
+                    );
                 }
 
-            } */
-        } 
-         /* private void Main_Picturebox_Click(object sender, EventArgs e)
+            }
+        }
+        private void Main_Picturebox_Click(object sender, EventArgs e)
         {
 
             if (ImageListview.FocusedItem != null)
@@ -89,7 +91,6 @@ namespace ImageViwer_Beta
 
             }
         }
-          */
         public Image photobox
         {
             set
@@ -101,8 +102,6 @@ namespace ImageViwer_Beta
         }
         private void bttn_fullsize_Click(object sender, EventArgs e)
         {
-
-            /* Usless code , save this for reference!!
             if (ImageListview.FocusedItem != null)
             {
                 try
@@ -117,9 +116,13 @@ namespace ImageViwer_Beta
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show("Invalid File! Please Make sure to choose the valid file!");
+                    MessageBox.Show(this, ex.Message, "Invalid File! Please Make sure to choose the valid file!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information
+                    );
                 }
             }
+            /* Usless code , save this for reference!!
+        
              * private void PictureBox_Click(object sender, EventArgs e)
             {
                 OpenFileDialog open = new OpenFileDialog();
@@ -134,7 +137,7 @@ namespace ImageViwer_Beta
         }
         private void button1_Click(object sender, EventArgs e)
         {
-           /* string[] infoImage = new string[40];
+            string[] infoImage = new string[40];
             int i = 0;
             string toDisplay = "";
             try
@@ -170,9 +173,9 @@ namespace ImageViwer_Beta
             catch (ImageProcessingException)
             {
                 MessageBox.Show("Invalid Digital Image file!");
-            } */
-        }
-               
+            } 
+            }
+
         private void MainPictureBox_Click(object sender, EventArgs e)
         {
 
@@ -185,123 +188,21 @@ namespace ImageViwer_Beta
                 {
                     //set minimum size to zoom
                     if (MainPictureBox.Width < 50)
+                        // lbl_Zoom.Text = pictureBox1.Image.Size; 
                         return;
                 }
                 else
                 {
                     //set maximum size to zoom
-                    if (MainPictureBox.Width > 10000)
+                    if (MainPictureBox.Width > 5000)
                         return;
                 }
-                MainPictureBox.Width += Convert.ToInt32(MainPictureBox.Width * e.Delta / 10000);
-                MainPictureBox.Height += Convert.ToInt32(MainPictureBox.Height * e.Delta / 10000);
+                MainPictureBox.Width += Convert.ToInt32(MainPictureBox.Width * e.Delta / 5000);
+                MainPictureBox.Height += Convert.ToInt32(MainPictureBox.Height * e.Delta / 5000);
             }
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void openPhotoMenu_Click(object sender, EventArgs e)
-        {
-            
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Multiselect = true;
-            openFileDialog.Filter = "JPEG|*.jpg|Bitmaps|*.bmp|GIF|*.gif|TIFF|*.tiff|PNG|*.png";
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                imagelist.Clear();
-                //ImageListview.Items.Clear();
-                    pFileNames = openFileDialog.FileNames;
-                    PhotoList listphoto = new PhotoList();
-                    listphoto.ShowingArray(pFileNames);
-                    pCurrentImage = 0;
-                   
-            }
-        }
-
-        protected void ShowCurrentImage()
-        {
-            foreach (string image in pFileNames)
-            {
-                if (pCurrentImage >= 0 && pCurrentImage <= pFileNames.Length - 1)
-                {
-
-                    MainPictureBox.Image = Bitmap.FromFile(pFileNames[pCurrentImage]);
-
-                }
-            }
-        }
-
-        private void prev_btn_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                    if (pFileNames.Length > 0)
-                    {
-                        pCurrentImage = pCurrentImage == 0 ? pFileNames.Length - 1 : --pCurrentImage;
-                        ShowCurrentImage();
-                    }
-            }
-            catch (Exception ex)
-            {
-            }
-
-        }
-
-        private void next_btn_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (pFileNames.Length > 0)
-                {
-                    pCurrentImage = pCurrentImage == pFileNames.Length - 1 ? 0 : ++pCurrentImage;
-                    ShowCurrentImage();
-                }
-            }
-            catch (Exception ex)
-            {
-            }
-        }
-
-        private void fullSizeImageToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                using (Photo phto = new Photo())
-                {
-                    //Image img = Image.FromFile(imagelist[ImageListview.FocusedItem.Index]);
-                    Image img = MainPictureBox.Image;
-                    phto.imagebox = img;
-                    phto.ShowDialog();
-
-                }
-            }
-            catch (Exception ex)
-               {
-                MessageBox.Show("Invalid File! Please Make sure to choose the valid file!");
-            }
-        }
-
-        private void selectedImagesToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            PhotoList listphoto = new PhotoList();
-            listphoto.Show();
-        }
-        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-            
-        }
-
-        private void PassArrayToPhotolist()
-        {
-            if (pFileNames.Length > 0){
-                
-                PhotoList listphoto = new PhotoList();
-                listphoto.ShowingArray(pFileNames);
-            }
-        }
        
+
         }
     }
